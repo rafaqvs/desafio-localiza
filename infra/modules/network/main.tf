3108d7ad499170429aed1e656f9b217aa0f634fa
rafaqvs <rafaqvs@gmail.com>
2025-04-10 16:25:01 -0300

    Update main.tf

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "this" {
+resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -8,20 +8,11 @@ resource "aws_vpc" "this" {
   }
 }
 
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
-
-  tags = {
-    Name = "main-igw"
-  }
-}
-
-# Subnets públicas
 resource "aws_subnet" "public" {
-  count                   = length(var.azs)
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
-  availability_zone       = element(var.azs, count.index)
+  count             = length(var.azs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, count.index)
+  availability_zone = var.azs[count.index]
   map_public_ip_on_launch = true
 
   tags = {
@@ -29,46 +20,11 @@ resource "aws_subnet" "public" {
   }
 }
 
-# Subnets privadas para apps (EKS)
 resource "aws_subnet" "private" {
   count             = length(var.azs)
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 10)
-  availability_zone = element(var.azs, count.index)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, count.index + 3)
+  availability_zone = var.azs[count.index]
 
   tags = {
     Name = "private-subnet-${count.index + 1}"
-  }
-}
-
-# Subnets para banco de dados
-resource "aws_subnet" "database" {
-  count             = length(var.azs)
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 20)
-  availability_zone = element(var.azs, count.index)
-
-  tags = {
-    Name = "db-subnet-${count.index + 1}"
-  }
-}
-
-# Route Table pública + associação
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.this.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this.id
-  }
-
-  tags = {
-    Name = "public-rt"
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
-}
