bb682bc0ce5ace4056878e60347280306bf781dc
rafaqvs <rafaqvs@gmail.com>
2025-04-10 16:20:26 -0300

    Update variables.tf

--- infra/modules/rds/variables.tf
+++ infra/modules/rds/variables.tf
@@ -1,21 +1,33 @@
-variable "private_subnet_ids" {
+variable "db_subnet_ids" {
   type = list(string)
 }
 
-variable "sg_id" {
+variable "vpc_id" {
   type = string
 }
 
-variable "db_name" {
-  type    = string
-  default = "appdb"
-}
-
-variable "db_user" {
+variable "db_username" {
   type    = string
   default = "admin"
 }
 
 variable "db_password" {
-  type    = string
+  type      = string
+  sensitive = true
 }
+
+variable "engine" {
+  type    = string
+  default = "mysql"
+}
+
+variable "instance_class" {
+  type    = string
+  default = "db.t3.micro"
+}
+
+variable "db_name" {
+  type    = string
+  default = "localiza"
+}
+
