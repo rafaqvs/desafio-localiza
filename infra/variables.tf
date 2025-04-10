f853b9563c64ca2cd9fdde6b6690893526a651cc
rafaqvs <rafaqvs@gmail.com>
2025-04-10 16:19:36 -0300

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,32 @@
+variable "db_subnet_ids" {
+  type = list(string)
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "db_username" {
+  type    = string
+  default = "admin"
+}
+
 variable "db_password" {
-  description = "Senha do banco de dados"
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
+}
+
+variable "engine" {
+  type    = string
+  default = "mysql"
+}
+
+variable "instance_class" {
+  type    = string
+  default = "db.t3.micro"
+}
+
+variable "db_name" {
+  type    = string
+  default = "localiza"
 }
