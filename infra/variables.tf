d69f1972699f0e4dab648efca1a98f170345455c
rafaqvs <rafaqvs@gmail.com>
2025-04-10 16:23:54 -0300

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,32 +1,5 @@
-variable "db_subnet_ids" {
-  type = list(string)
-}
-
-variable "vpc_id" {
-  type = string
-}
-
-variable "db_username" {
-  type    = string
-  default = "admin"
-}
-
-variable "db_password" {
-  type      = string
-  sensitive = true
-}
-
-variable "engine" {
-  type    = string
-  default = "mysql"
-}
-
-variable "instance_class" {
-  type    = string
-  default = "db.t3.micro"
-}
-
-variable "db_name" {
-  type    = string
-  default = "localiza"
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
