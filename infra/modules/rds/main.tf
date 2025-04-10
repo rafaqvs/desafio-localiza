8260793db652b419c0400390524342aaeedf0a4e
rafaqvs <rafaqvs@gmail.com>
2025-04-10 16:20:44 -0300

    Update main.tf

--- infra/modules/rds/main.tf
+++ infra/modules/rds/main.tf
@@ -1,26 +1,49 @@
 resource "aws_db_subnet_group" "this" {
-  name       = "rds-subnet-group"
-  subnet_ids = var.private_subnet_ids
+  name       = "main-db-subnet-group"
+  subnet_ids = var.db_subnet_ids
 
   tags = {
-    Name = "rds-subnet-group"
+    Name = "main-db-subnet-group"
+  }
+}
+
+resource "aws_security_group" "rds" {
+  name        = "rds-sg"
+  description = "SG for RDS"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"] # Ajuste de acordo com sua VPC
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-sg"
   }
 }
 
 resource "aws_db_instance" "this" {
-  allocated_storage    = 20
-  engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t3.micro"
-  name                 = var.db_name
-  username             = var.db_user
-  password             = var.db_password
-  port                 = 3306
+  identifier             = "localiza-db"
+  allocated_storage      = 20
+  engine                 = var.engine
+  instance_class         = var.instance_class
+  name                   = var.db_name
+  username               = var.db_username
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.this.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  skip_final_snapshot    = true
 
-  db_subnet_group_name    = aws_db_subnet_group.this.name
-  vpc_security_group_ids  = [var.sg_id]
-  skip_final_snapshot     = true
-  publicly_accessible     = false
-  multi_az                = false
-  deletion_protection     = false
+  tags = {
+    Name = "localiza-db"
+  }
 }
