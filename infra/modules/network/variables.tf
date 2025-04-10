063b98fb102b457d85f737ef186ca04797f24759
rafaqvs <rafaqvs@gmail.com>
2025-04-10 16:25:41 -0300

    Update variables.tf

--- infra/modules/network/variables.tf
+++ infra/modules/network/variables.tf
@@ -1,10 +1,9 @@
 variable "vpc_cidr" {
   type        = string
-  default     = "10.0.0.0/16"
   description = "CIDR da VPC"
 }
 
 variable "azs" {
   type        = list(string)
-  description = "Zonas de disponibilidade"
+  description = "Availability Zones"
 }
