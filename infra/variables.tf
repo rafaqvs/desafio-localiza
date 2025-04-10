ce2b9cca2f099e8126f06c3baa6679347155b9c7
rafaqvs <rafaqvs@gmail.com>
2025-04-10 16:32:56 -0300

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,29 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "azs" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "db_name" {
+  type    = string
+  default = "localiza"
+}
+
+variable "db_username" {
+  type    = string
+  default = "admin"
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
 }
